37fc27cccb06f6de56231f643dc324ac35b8196d
Tuấn Đoàn <doanminhtuan.hd@gmail.com>
2017-12-23 04:45:17 +0700

    Update InteractiveDataProfile.php
    Update hàm GetProfileWithUser để select dữ liệu trên database và kiểm tra xem có khớp với input của người dùng

--- models/InteractiveDataProfile.php
+++ models/InteractiveDataProfile.php
@@ -12,15 +12,15 @@ class InteractiveDataProfile extends CI_Model {
 	public function GetProfileWithOther($IdProfile){
 		$this->mongodb->select();
 	}
-	public function GetProfileWithUser($Email,$Password){
+	public function GetProfileWithUser($Email, $Password) {
 		$this->load->library('mongodb');
-		$Temp = array('email'=>$Email,'password'=>$Password);
-		$result = $this->mongodb->where($Temp)->get('profile');
-		//$result = $result->result_array(); // chỉ dùng với dữ liệu dùng hệ quản trị mysql hoặc sql server
-		//echo "<pre>";
-		//var_dump($result);
+		$data = array(
+		    "email" => $Email,
+		    "password" => $Password
+		);
+		$result = $this->mongodb->where($data)->get('profile');
 		return $result;
-	}
+    	}
 	public function InsertProfile($name,$email,$password,$birth,$hometown,$highschool,$unversity){
 
 	}
