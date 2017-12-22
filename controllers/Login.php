a9e645339d1afc4768bae7a783f8df49e21cd490
ngominhquang96 <32742113+ngominhquang96@users.noreply.github.com>
2017-12-22 13:26:04 +0700

    update /controller/Login

--- controllers/Login.php
+++ controllers/Login.php
@@ -9,9 +9,19 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('Login');
+		$this->load->model('InteractiveDataProfile');
+	}
+	public function CheckInformation(){
+		$Email= $this->input->post('Email');
+		$Password=$this->input->post('Password');
+		$partten = "/^[A-Za-z0-9_\.]{6,32}@([a-zA-Z0-9]{2,12})(\.[a-zA-Z]{2,12})+$/";
+		if (preg_match($partten ,$Email, $matchs)) {
+			$result= $this->InteractiveDataProfile->GetProfile($Email,$Pasword);
+			// sau đó kiểm tra xem result trả về cái gì , nếu tồn tại thì load đên trang newfeed 
+		}
 		
 	}
-
 }
 
 /* End of file Login.php */
