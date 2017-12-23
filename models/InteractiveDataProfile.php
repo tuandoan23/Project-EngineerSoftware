e89d8603e34e25379b556ca4702ba061892c45c5
Tuấn Đoàn <doanminhtuan.hd@gmail.com>
2017-12-23 16:01:55 +0700

    Update InteractiveDataProfile.php
    Update hàm InsertProfile khi tạo tài khoản mới,  hàm GetProfileWithUser_CheckExist để kiểm tra xem email khi đăng ký đã tồn tại hay chưa

--- models/InteractiveDataProfile.php
+++ models/InteractiveDataProfile.php
@@ -1,32 +1,58 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class InteractiveDataProfile extends CI_Model {
 
-	public $variable;
+    public $variable;
 
-	public function __construct()
-	{
-		parent::__construct();
-		
-	}
-	public function GetProfileWithOther($IdProfile){
-		$this->mongodb->select();
-	}
-	public function GetProfileWithUser($email_input, $password_input) {
-		$this->load->library('mongodb');
-		$data = array(
-		    "email" => $email_input,
-		    "password" => $password_input
-		);
-		$result = $this->mongodb->where($data)->get('profile');
-		return $result;
-	}
-	public function InsertProfile($name,$email,$password,$birth,$hometown,$highschool,$unversity){
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('mongodb');
+    }
 
-	}
-	public function UdateProfile($IdProfile){
-		
-	}
+    public function GetProfileWithOther($IdProfile) {
+        $this->mongodb->select();
+    }
+
+    public function GetProfileWithUser($Email, $Password) {
+        $this->load->library('mongodb');
+        $data = array(
+            "email" => $Email,
+            "password" => $Password
+        );
+        $result = $this->mongodb->where($data)->get('profile');
+
+        return $result;
+    }
+
+    public function GetProfileWithUser_CheckExist($Email) {
+        $data = array(
+            "email" => $Email
+        );
+        $result = $this->mongodb->where($data)->get('profile');
+        return $result;
+    }
+
+    public function InsertProfile($name, $email, $password, $birth, $hometown, $highschool, $university) {
+        $data = array(
+            "name" => $name,
+            "email" => $email,
+            "password" => $password,
+            "birth" => $birth,
+            "hometown" => $hometown,
+            "highschool" => $highschool,
+            "university" => $university
+        );
+        $result = $this->mongodb->insert('profile', $data);
+        return $result;
+    }
+
+    public function UdateProfile($IdProfile) {
+        
+    }
+
 }
 
 /* End of file InteractiveDataProfile.php */
